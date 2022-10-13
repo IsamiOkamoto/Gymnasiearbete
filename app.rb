31646aa12e04768655ca7ac0a074ec11ac4448a6
IsamiOkamoto <isami.okamotogustafsson@elev.ga.ntig.se>
2022-10-13 16:34:34 +0200

    Torsdag 13/10

--- app.rb
+++ app.rb
@@ -134,6 +134,27 @@ def similar_length(word)
     end
     return output
 end
+def not_in_array(word, arr)
+    i = 0
+    while i < arr.length
+        if word == arr[i].length
+            return false
+        end
+        i += 1
+    end
+    return true
+end
+def letters_compare(arr, word_arr) #kolla så output är en float
+    i = 0
+    output = 0
+    while i < arr.length
+        if not_in_array(arr[i], word_arr) == false
+            output += 1
+        end
+        i += 1
+    end
+    return output/word_arr.length
+end
 def find_word_2(word, arr)
     i = 0
     correct = []
@@ -174,7 +195,7 @@ def find_word_2(word, arr)
                     if arr[i][j] == word[j] or arr[i][j] == word[j + 1]
                         correct[i] += 1
                     end
-                elsif j == word.length - 1:
+                elsif j == word.length - 1
                     if arr[i][j - 1] == word[j] or arr[i][j - 1] == word[j - 1]
                         correct[i] += 1
                     end
@@ -191,49 +212,59 @@ def find_word_2(word, arr)
     i = 0
     output = []
     while i < correct.length
-        if correct[i]/word.length >= 0.75:
+        if correct[i]/word.length >= 0.75
             output.append(arr[i])
         end
         i += 1
     end
     return output
 end
-def find_word_v1(word, arr):
+def find_word_v1(word, arr)
     i = 0
     correct = []
-    while i < len(arr):
+    while i < arr.length
         correct.append(0)
         j = 0
-        while j < len(arr[i]):
-            if len(arr[i]) == len(word):
-                if j == 0:
-                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j]:
+        while j < arr[i].length
+            if arr[i].length == word.length
+                if j == 0
+                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j]
                         correct[i] += 1
-                elif j == len(arr[i]) - 1:
-                    if arr[i][j] == word[j] or arr[i][j - 1] == word[j]:
+                    end
+                elsif j == arr[i].length - 1
+                    if arr[i][j] == word[j] or arr[i][j - 1] == word[j]
                         correct[i] += 1
-                else:
-                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j] or arr[i][j - 1] == word[j]:
+                    end
+                else
+                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j] or arr[i][j - 1] == word[j]
                         correct[i] += 1
-            elif len(arr[i]) + 1 == len(word):
-                if j == 0:
-                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j]:
+                    end
+                end
+            elsif len(arr[i]) + 1 == len(word)
+                if j == 0
+                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j]
                         correct[i] += 1
-                elif j == len(arr[i]) - 1:
-                    if arr[i][j] == word[j] or arr[i][j - 1] == word[j]:
+                    end
+                elsif j == arr[i].length - 1
+                    if arr[i][j] == word[j] or arr[i][j - 1] == word[j]
                         correct[i] += 1
-                else:
-                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j] or arr[i][j - 1] == word[j]:
+                    end
+                else
+                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j] or arr[i][j - 1] == word[j]
                         correct[i] += 1
-            elif len(arr[i]) - 1 == len(word):
-                if j == 0:
-                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j]:
+                    end
+                end
+            elsif arr[i].length - 1 == word.length
+                if j == 0
+                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j]
                         correct[i] += 1
-                elif j == len(arr[i]) - 1:
-                    if arr[i][j] == word[j - 1]: #questionable
+                    end
+                elsif j == arr[i].length - 1
+                    if arr[i][j] == word[j - 1] 
                         correct[i] += 1
-                else:
-                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j] or arr[i][j - 1] == word[j]:
+                    end
+                else
+                    if arr[i][j] == word[j] or arr[i][j + 1] == word[j] or arr[i][j - 1] == word[j]
                         correct[i] += 1
                     end
                 end
@@ -245,10 +276,147 @@ def find_word_v1(word, arr):
     i = 0
     output = []
     while i < correct.length
-        if correct[i]/word.length >= 0.75:
+        if correct[i]/word.length >= 0.75
             output.append(arr[i])
         end
         i += 1
     end
     return output
 end
+def find_word_main(word, arr)
+    arr1 = find_word_v1(word, arr)
+    arr2 = find_word_v2(word, arr)
+    priority = []
+    i = 0
+    while i < arr1.length
+        j = 0
+        while j = arr2.length
+            if arr1[i] == arr2[j]
+                if not_in_array(arr1[i], priority)
+                    priority.append(arr1[i])
+                end
+            end
+            j += 1
+        end
+        i += 1
+    end
+    if arr2.length < arr1.length
+        arr3 = arr2 + arr1
+    else
+        arr3 = arr1 + arr2
+    end
+    i = 0
+    while i arr3.length
+        if not_in_array(arr3[i], priority)
+            priority.append(arr3[i])
+        end
+        i += 1
+    end
+    return priority
+end
+def similar_letters(word, arr)
+    i = 0
+    letters = []
+    while i < word.length
+        if not_in_array(word[i], letters)
+            letters.append(word[i])
+        end
+        i += 1
+    end
+    i = 0
+    arr_letters = []
+    while i < arr.length
+        j = 0
+        arr_letters.append([])
+        while j < arr[i].length
+            if not_in_array(arr[i][j], arr_letters[i])
+                arr_letters.append(arr[i][j])
+            end
+            j += 1
+        end
+        i += 1
+    end
+    output = []
+    correct = []
+    i = 0
+    while i arr.length
+        correct.append(0)
+        correct[i] = letters_compare(arr_letters[i], etters)
+        i += 1
+    end
+    i = 0
+    if word.length >= 4
+        while i < correct.length
+            if correct[i] >= 0.75
+                output.append(arr[i])
+            end
+            i += 1
+        end
+    else
+        while i < correct.length
+            if correct[i] >= 0.67
+                output.append(arr[i])
+            end
+            i += 1
+        end
+    end
+    return output
+end
+def similar_letters_v2(word, arr)
+    i = 0
+    letters = []
+    while i < word.length
+        if not_in_array(word[i], letters)
+            letters.append(word[i])
+        end
+        i += 1
+    end
+    i = 0
+    arr_letters = []
+    while i < arr.length
+        j = 0
+        arr_letters.append([])
+        while j < arr[i].length
+            if not_in_array(arr[i][j], arr_letters[i])
+                arr_letters[i].append(arr[i][j])
+            end
+            j += 1
+        end
+        i += 1
+    end
+    i = 0
+    output = []
+    correct =[]
+    while i < arr.length
+        correct.append(0)
+        correct[i] = letters_compare(letters, arr_letters[i])
+        i += 1
+    end
+    i = 0
+    if word.length >= 4
+        while i < correct.length
+            if correct[i] >= 0.75
+                output.append(arr[i])
+            end
+            i += 1
+        end
+    else
+        while i < correct.length
+            if correct[i] > 0.67
+                output.append(arr[i])
+            end
+            i += 1
+        end
+    end
+    return output
+end
+def spelling(word)
+    word = word.lowercase
+    i = similar_length(word)
+    arr = find_word_main(word, i)
+    output = similar_letters(word, arr)
+    output = similar_letters_v2(word, output)
+    return output
+end
+get(idk) do #shiritori
+#testning
